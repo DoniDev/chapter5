8759a1ac97cdc6c37c5fd13ff05a72b74623b49f
DoniDev <abduvokhidovdoniyor4@gmail.com>
2020-06-01 07:23:14 +0500

    testing done

--- blogapp/tests.py
+++ blogapp/tests.py
@@ -1,3 +1,47 @@
 from django.test import TestCase
+from . models import Post
+from django.urls import reverse
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+class BlogTest(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username = 'testuser',
+            email = 'test@gmail.com',
+            password = 'testing321',
+        )
+
+        self.post = Post.objects.create(
+            user=self.user,
+            title='hello',
+            body='body part'
+        )
+
+    def test_string_representation(self):
+        post = Post(title='A sample title')
+        self.assertEqual(str(post), post.title)
+
+    def test_post_content(self):
+        self.assertEqual(f'{self.post.title}', 'hello')
+        self.assertEqual(f'{self.post.user}', 'testuser')
+        self.assertEqual(f'{self.post.body}', 'body part')
+
+    def test_post_list_view(self):
+        resp = self.client.get(reverse('home'))
+        self.assertEqual(resp.status_code, 200)
+        self.assertContains(resp, 'body part')
+        self.assertTemplateUsed(resp, 'home/home.html')
+
+    def test_post_detail_view(self):
+        response = self.client.get('/post/1/')
+        no_response = self.client.get('/post/100000/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(no_response.status_code, 404)
+        self.assertContains(response, 'body part')
+        self.assertTemplateUsed(response, 'home/detail.html')
+
+
+
+
+
+
--- blogapp/views.py
+++ blogapp/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from . models import Post
 
-# Create your views here.
+
+class BlogListView(ListView):
+    model = Post
+    # context_object_name = 'posts'
+    template_name = 'home/home.html'
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'home/detail.html'
+
+
+
